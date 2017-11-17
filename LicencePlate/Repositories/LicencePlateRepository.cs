97e442090095ff2cc17a9fc9821c807e10d69421
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-17 11:44:55 +0100

    OnlyPoliceCars function created

--- LicencePlate/Repositories/LicencePlateRepository.cs
+++ LicencePlate/Repositories/LicencePlateRepository.cs
@@ -34,5 +34,13 @@ namespace LicencePlate.Repositories
                            select plate;
             return selected.ToList();
         }
+
+        public List<LicencePlateClass> OnlyPoliceCars(string licenceplate)
+        {
+            var policeCars = from plate in LicencePlateContext.licence_plates
+                             where plate.plate.StartsWith("RB")
+                             select plate;
+            return policeCars.ToList();
+        }
     }
 }
