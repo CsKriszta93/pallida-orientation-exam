5bdf8e16c6dbf29397855213ddb6671d05bce29c
CsKriszta93 <kriszta.csere93@gmail.com>
2017-11-17 10:54:25 +0100

    Input field and search button created

---
+++ LicencePlate/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LicencePlate.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LicencePlate.Controllers
+{
+    public class HomeController : Controller
+    {
+        LicencePlateRepository LicencePlateRepository;
+
+        public HomeController(LicencePlateRepository licencePlateRepository)
+        {
+            LicencePlateRepository = licencePlateRepository;
+        }
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
